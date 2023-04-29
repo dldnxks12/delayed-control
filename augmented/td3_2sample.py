928da82161c1c7c19b10de8eb28c1a3fb2ada576
Jongsoo Lee <jjongs@postech.ac.kr>
2023-04-29 13:39:00 +0900

    Update td3_2sample.py

--- augmented/td3_2sample.py
+++ augmented/td3_2sample.py
@@ -18,6 +18,7 @@ import torch.nn.functional as F
 
 from collections import deque
 
+
 class ReplayBuffer():
     def __init__(self):
         self.buffer = deque(maxlen=20000)
@@ -27,46 +28,37 @@ class ReplayBuffer():
 
     def sample(self, batch_size):
         samples = random.sample(self.buffer, batch_size)
-        states, bbactions, bactions, actions, rewards, next_states, terminateds, truncateds = [], [], [], [], [], [], [], []
 
-        for state, bbaction, baction, action, reward, next_state, terminated, truncated in samples:
-            states.append(state.cpu().numpy())
+        Is, actions, rewards, I_nexts, terminateds = [], [], [], [], []
 
-            bbactions.append(bbaction.cpu().detach().numpy())
-            bactions.append(baction.cpu().detach().numpy())
+        for I, action, reward, I_next, terminated in samples:
+            Is.append(I.cpu().numpy())
             actions.append(action.cpu().detach().numpy())
-
             rewards.append(reward)
-            next_states.append(next_state.cpu().numpy())
+            I_nexts.append(I_next.cpu().numpy())
             terminateds.append(terminated)
-            truncateds.append(truncated)
 
-        states      = torch.tensor(states, device = device)
-        
-        bbactions   = torch.tensor(bbactions, device=device)
-        bactions    = torch.tensor(bactions, device=device)
+        Is          = torch.tensor(Is, device = device)
         actions     = torch.tensor(actions, device=device)
-        
         rewards     = torch.tensor(rewards, device=device)
-        next_states = torch.tensor(next_states, device=device)
+        I_nexts     = torch.tensor(I_nexts, device=device)
         terminateds = torch.tensor(terminateds, device=device)
-        truncateds  = torch.tensor(truncateds, device=device)
 
-        return states, bbactions, bactions, actions, rewards, next_states, terminateds, truncateds
+        return Is, actions, rewards, I_nexts, terminateds
 
     def size(self):
         return len(self.buffer)
 
 
+
 class QNetwork(nn.Module):
     def __init__(self):
         super(QNetwork, self).__init__()
-        self.fc_s   = nn.Linear(7, 256)
-        self.fc_a   = nn.Linear(1, 256)
+        self.fc_s   = nn.Linear(9, 128)
+        self.fc_a   = nn.Linear(1, 128)
 
-        self.fc1    = nn.Linear(512, 256)
-        self.fc2    = nn.Linear(256, 64)
-        self.fc_out = nn.Linear(64, 1)
+        self.fc1    = nn.Linear(256, 128)
+        self.fc2    = nn.Linear(128, 1)
 
     def forward(self, state, action):
 
@@ -76,21 +68,19 @@ class QNetwork(nn.Module):
         concatenate = torch.cat([h1, h2], dim = -1)
 
         q  = F.relu(self.fc1(concatenate))
-        q  = F.relu(self.fc2(q))
+        q  = self.fc2(q)
 
-        return self.fc_out(q)
+        return q
 
 class PolicyNetwork(nn.Module):
     def __init__(self):
         super(PolicyNetwork, self).__init__()
-        self.fc1 = nn.Linear(9, 256) # Input : State 3
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 1)  # Output : Action 1
+        self.fc1 = nn.Linear(9, 128)
+        self.fc2 = nn.Linear(128, 1)
 
     def forward(self, state):
         state  = F.relu(self.fc1(state))
-        state  = F.relu(self.fc2(state))
-        action = torch.tanh(self.fc3(state)) # torque range : [-1 ~ 1]
+        action = torch.tanh(self.fc2(state)) # torque range : [-1 ~ 1]
 
         return action
 
@@ -108,8 +98,10 @@ class OrnsteinUhlenbeckNoise:
         return x
 
 def train(Buffer, Q1, Q1_target, Q2, Q2_target, Pi, Pi_target, Q1_optimizer, Q2_optimizer, Pi_optimizer):
-    states, bbactions, bactions, actions, rewards, next_states, terminateds, truncateds = Buffer.sample(batch_size)
+    Is, actions, rewards, I_nexts, terminateds = Buffer.sample(batch_size)
 
+    Is      = Is.squeeze(1)
+    I_nexts = I_nexts.squeeze(1)
     terminateds = torch.unsqueeze(terminateds.type(torch.FloatTensor).to(device), dim = 1)
     rewards     = torch.unsqueeze(rewards, dim = 1)
 
@@ -117,17 +109,15 @@ def train(Buffer, Q1, Q1_target, Q2, Q2_target, Pi, Pi_target, Q1_optimizer, Q2_
 
     with torch.no_grad():
 
-        I_next = torch.concatenate([next_states, bactions] , dim = 1)
-        I_next = torch.concatenate([I_next, actions],        dim=1)
-        action_bar = Pi_target(I_next)
+        action_bar = Pi_target(I_nexts)
 
-        q1_value = Q1_target(next_states, action_bar)
-        q2_value = Q2_target(next_states, action_bar)
+        q1_value = Q1_target(I_nexts, action_bar)
+        q2_value = Q2_target(I_nexts, action_bar)
 
         y = rewards + ( gamma * torch.minimum(q1_value, q2_value) * (1 - terminateds))
 
-    Q1_loss = ( (y - Q1(states, actions)) ** 2 ).mean()
-    Q2_loss = ( (y - Q2(states, actions)) ** 2 ).mean()
+    Q1_loss = ( (y - Q1(Is, actions)) ** 2 ).mean()
+    Q2_loss = ( (y - Q2(Is, actions)) ** 2 ).mean()
 
     Q1_optimizer.zero_grad()
     Q1_loss.backward()
@@ -137,14 +127,10 @@ def train(Buffer, Q1, Q1_target, Q2, Q2_target, Pi, Pi_target, Q1_optimizer, Q2_
     Q2_loss.backward()
     Q2_optimizer.step()
 
-
     for p in Q1.parameters():
         p.requires_grad = False
 
-    I_cur = torch.concatenate([states, bbactions], dim=1)
-    I_cur = torch.concatenate([I_cur, bactions],  dim=1)
-
-    pi_loss = - Q1(states, Pi(I_cur)).mean()
+    pi_loss = - Q1(Is, Pi(Is)).mean()
 
     Pi_optimizer.zero_grad()
     pi_loss.backward()
@@ -173,9 +159,9 @@ print("")
 
 lr_pi = 0.0001
 lr_q  = 0.001
-tau   = 0.001 
-gamma = 0.99  
-batch_size = 32
+tau   = 0.001
+gamma = 0.99
+batch_size = 64
 
 # Q function
 Q1 = QNetwork().to(device)
@@ -214,7 +200,7 @@ for episode in range(MAX_EPISODE):
     # Basic Learning Setting
     state, _   = current_env.reset()
     state      = torch.tensor(state).float().to(device)
-    terminated, truncated = False, False
+    terminated = False
     total_reward = 0
 
     # Delayed Action Setting
@@ -223,43 +209,38 @@ for episode in range(MAX_EPISODE):
     # Generate Episodes ...
     for time_step in range(max_time_step):
         with torch.no_grad():
-            if episode < 2:
-                action = current_env.action_space.sample()
-                action = torch.FloatTensor(action).to(device)
+            if len(Action_buffer) < 3: # Padding with 0
+                I = torch.concatenate([state.unsqueeze(0), torch.FloatTensor([0, 0]).unsqueeze(0).to(device)], dim=1)
             else:
-                # action delay -> Ik = (Sk , A_k-d, A_k-d+1, A_k-d+2, ...., A_k-1)
-                if len(Action_buffer) < 3: # Padding with 0
-                    I = torch.concatenate([state.unsqueeze(0), torch.FloatTensor([0, 0]).unsqueeze(0).to(device)], dim=1)
+                I = torch.concatenate([state.unsqueeze(0), torch.FloatTensor([Action_buffer[-2], Action_buffer[-1]]).unsqueeze(0).to(device)] , dim = 1)
 
-                else:
-                    # (S_t, A_t-2, A_t-1) S_t에서 A_t를 apply해야하는데, A_t-3을 apply할 것 --- 2 sample delay
-                    I = torch.concatenate([state.unsqueeze(0), torch.FloatTensor([Action_buffer[-3], Action_buffer[-2]]).unsqueeze(0).to(device)] , dim = 1)
-
-                action = torch.clamp( (Pi(I) + noise()[0]), -1, 1 )[0]
-                #action = Pi(I)
-
-        Action_buffer.append(action)
+            #action = torch.clamp( (Pi(I) + noise()[0]), -1, 1 )[0]
+            action = Pi(I)[0]
 
         if len(Action_buffer) < 3: # gathering actions
-            next_state, reward, terminated, truncated, _ = current_env.step(action.cpu().detach().numpy())
+            next_state, reward, terminated, _, _ = current_env.step(action.cpu().detach().numpy())
             next_state = torch.tensor(next_state).float().to(device)
             state = next_state
+            Action_buffer.append(action)
             continue
-            
+
         else: # apply delayed action
-            next_state, reward, terminated, truncated, _ = current_env.step(Action_buffer[-3].cpu().detach().numpy()) # Ok env.step(A_k-1)
+            next_state, reward, terminated, _, _ = current_env.step(Action_buffer[-2].cpu().detach().numpy())
             next_state = torch.tensor(next_state).float().to(device)
+            I_next = torch.concatenate([next_state.unsqueeze(0), torch.FloatTensor([Action_buffer[-1], action]).unsqueeze(0).to(device)],dim=1)
+            Action_buffer.append(action)
 
         terminated = not terminated if time_step == max_time_step - 1 else terminated
 
         # s0, s1, s2, s3, a1, r, s4
-        Buffer.put([state, Action_buffer[-3], Action_buffer[-2], Action_buffer[-1], reward, next_state, terminated, truncated])
+        Buffer.put([I, action, reward, I_next, terminated])
         total_reward += reward
 
-        if Buffer.size() > 50: # Train Q, Pi
+        if Buffer.size() > 500: # Train Q, Pi
+            #for _ in range(5):
             train(Buffer, Q1, Q1_target, Q2, Q2_target, Pi, Pi_target, Q1_optimizer, Q2_optimizer, Pi_optimizer)
 
-        if terminated or truncated:
+        if terminated :
             break
 
         state = next_state
@@ -270,5 +251,5 @@ for episode in range(MAX_EPISODE):
 env.close()
 
 Y = np.array(Y)
-np.save('./TD3_delayed_2sample4', Y)
+np.save('./TD3_delayed_2sample', Y)
 
