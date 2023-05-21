77eb6894cd802125eb484669edd39021899512e6
Jongsoo Lee <jjongs@postech.ac.kr>
2023-05-21 17:29:50 +0900

    Create ppo_continuous_main.py

---
+++ ppo_p/ppo_continuous_main.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import gymnasium as gym
+import torch.cuda
+import ppo_agent
+import environment
+
+def main():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # before validation process, use cpu
+    device = 'cpu'
+    print(f"device : {device}")
+
+    xml_file = os.getcwd() + "/environment/assets/inverted_single_pendulum.xml"
+
+    max_episodes = 5000
+    #env_name     = "InvertedSinglePendulum-v4"
+    env_name = "Pendulum-v1"
+    env          = gym.make(env_name)#, model_path = xml_file)
+    agent        = ppo_agent.PPOagent(env, device)
+
+    agent.train(max_episodes)
+    agent.plot_result()
+
+if __name__ == "__main__":
+    main()
